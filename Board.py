5ee3950465285f325880622dc56eaf386a5bc1f2
Agnibho Mondal <mail@agnibho.com>
2015-12-07 19:35:37 +0530

    Added licensing information

--- Board.py
+++ Board.py
@@ -1,3 +1,21 @@
+# Agnibho's Game of Life - Python implementation of Conway's Game of Life
+# Copyright (C) 2014  Agnibho Mondal
+
+# This file is part of Agnibho's Game of Life
+
+# Agnibho's Game of Life is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Agnibho's Game of Life is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Agnibho's Game of Life.  If not, see <http://www.gnu.org/licenses/>.
+
 from Tkinter import BooleanVar, StringVar
 import Tkinter as tk
 import tkFileDialog
